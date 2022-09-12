e880001e5636e37a3ec1ed9a037eade8abd9cbef
Jacob Essex <git@JacobEssex.com>
2022-09-12 19:20:20 +0200

    Gem creation using bundler

---
+++ lib/jekyll/image/gallery.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "gallery/version"
+
+module Jekyll
+  module Image
+    module Gallery
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
